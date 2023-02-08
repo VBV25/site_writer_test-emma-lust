a6b28b93de4ab5bacb7c16a16dd752fc62bb916a
VBV25_2pc <vbv25boev@yandex.ru>
2023-02-08 23:09:05 +0300

    стандартная версия: хедер с адаптивом

--- standart-site-page/#source/js/script-standart-site.js
+++ standart-site-page/#source/js/script-standart-site.js
@@ -1,30 +1,12 @@
 /*----------------------ВСПЛЫВАЮЩИЕ ОКНА------------------------*/
 //
 $(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__navigation').toggleClass('active');
+    $('.header__btn-navigation-menu').click(function(event) {
+        $('.header__menu').toggleClass('active-block-menu');
         $('body').toggleClass('lock');
     });
     //
-    //-----попап Телефон
-    $('#header__phone-img2').click(function(event) {
-        $('.header-popup__phone-wrapper').addClass('visible');
-        $('body').addClass('lock');
-    });
-    $('.header-popup__btn-close-phone').click(function(event) {
-        $('.header-popup__phone-wrapper').removeClass('visible');
-        $('body').removeClass('lock');
-    });
-    //
-    //-----попап бронь
-    $('#demonstration-block__btn').click(function(event) {
-        $('.popup-reserv__wrapper').addClass('visible');
-        $('body').addClass('lock');
-    });
-    $('#popup-reserv__btn-close').click(function(event) {
-        $('.popup-reserv__wrapper').removeClass('visible');
-        $('body').removeClass('lock');
-    });
+
 
     /*--------------------------------------------------------------*/
 
