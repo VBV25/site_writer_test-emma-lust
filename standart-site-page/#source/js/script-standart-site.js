a52d59d54e674c395c3ca9209fc4ec6776d4e11b
VBV25_2 <vbv25boev@yandex.ru>
2023-02-27 19:44:10 +0300

    прелоадер

--- standart-site-page/#source/js/script-standart-site.js
+++ standart-site-page/#source/js/script-standart-site.js
@@ -1,67 +1,86 @@
 /*----------------------ВСПЛЫВАЮЩИЕ ОКНА------------------------*/
 //
-$(document).ready(function() {
-    $('.header__btn-navigation-menu').click(function(event) {
-        $('.header__menu').toggleClass('active-block-menu');
-        $('body').toggleClass('lock');
-    });
-    //
+$(document).ready(function () {
+  /*----------------прелоадер----------------*/
+  //скрытие прелоадера
+  if ($(window).on('load')) {
+    setTimeout(function () {
+      // $('#wrap_preloader').delay(2000).fadeToggle(300);
+      $('body').removeClass('lock');
+      $('.preloader').addClass('preloader-none');
+      $('.preloader').removeClass('preloader-visible');
+    }, 2000);
+  }
+  //
+  //
+  //
+  //
+  //
+  /*----------------------ВСПЛЫВАЮЩИЕ ОКНА------------------------*/
+  //
+  /*
+                        $('.header__btn-navigation-menu').click(function(event) {
+                            $('.header__menu').toggleClass('active-block-menu');
+                            $('body').toggleClass('lock');
+                        });
+                        //
 
 
-    /*--------------------------------------------------------------*/
+                        //
 
-    //name
-    $('#name').hover(function() {
-        var nameText = $('#name').val();
-        //
-        var nameError1 = 'The field is not filled';
-        var nameError2 = 'At least 2 characters';
-        var nameError3 = 'Only latin letters';
-        //
-        if (nameText == nameError1) {
-            console.log('yes');
-            $('#name').val('');
-        } else if (nameText == nameError2) {
-            console.log('yes2');
-            $('#name').val('');
-        } else if (nameText == nameError3) {
-            console.log('yes2');
-            $('#name').val('');
-        }
-    });
+                        //name
+                        $('#name').hover(function() {
+                            var nameText = $('#name').val();
+                            //
+                            var nameError1 = 'The field is not filled';
+                            var nameError2 = 'At least 2 characters';
+                            var nameError3 = 'Only latin letters';
+                            //
+                            if (nameText == nameError1) {
+                                console.log('yes');
+                                $('#name').val('');
+                            } else if (nameText == nameError2) {
+                                console.log('yes2');
+                                $('#name').val('');
+                            } else if (nameText == nameError3) {
+                                console.log('yes2');
+                                $('#name').val('');
+                            }
+                        });
 
-    //email
-    $('#email').hover(function() {
-        var emailText = $('#email').val();
-        //
-        var emailError1 = 'The field is not filled';
-        var emailError2 = 'Incorrect email';
-        //
-        if (emailText == emailError1) {
-            console.log('yes');
-            $('#email').val('');
-        } else if (emailText == emailError2) {
-            console.log('yes2');
-            $('#email').val('');
-        }
-    });
+                        //email
+                        $('#email').hover(function() {
+                            var emailText = $('#email').val();
+                            //
+                            var emailError1 = 'The field is not filled';
+                            var emailError2 = 'Incorrect email';
+                            //
+                            if (emailText == emailError1) {
+                                console.log('yes');
+                                $('#email').val('');
+                            } else if (emailText == emailError2) {
+                                console.log('yes2');
+                                $('#email').val('');
+                            }
+                        });
 
-    //text
-    $('#request').hover(function() {
-        var requestText = $('#request').val();
-        //
-        var requestError1 = 'The field is not filled';
-        var requestError2 = 'At least 2 characters';
+                        //text
+                        $('#request').hover(function() {
+                            var requestText = $('#request').val();
+                            //
+                            var requestError1 = 'The field is not filled';
+                            var requestError2 = 'At least 2 characters';
 
-        //
-        if (requestText == requestError1) {
-            console.log('yes');
-            $('#request').val('');
-        } else if (requestText == requestError2) {
-            console.log('yes2');
-            $('#request').val('');
-        }
-    });
+                            //
+                            if (requestText == requestError1) {
+                                console.log('yes');
+                                $('#request').val('');
+                            } else if (requestText == requestError2) {
+                                console.log('yes2');
+                                $('#request').val('');
+                            }
+                        });
 
-    //
+                        //
+                        */
 });
